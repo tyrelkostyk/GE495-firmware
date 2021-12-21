caf14bad6a66a03ce0e22868717a10da691038ab
Jeremy Rempel <jeremy.rempel@usask.ca>
2021-12-21 12:02:36 -0600

    add stubs and empty definitions for send, receive, parse functions and define command_t type

--- src/command.c
+++ src/command.c
@@ -2,3 +2,28 @@
 // Contains functions for parsing and carrying out commands received from downstream sources including ECU.
 
 #include "defs.h"
+
+/**
+ * Sends a command to the next upstream device.
+ */
+void cmdSendUpstream (void)
+{
+
+}
+
+/**
+ * Receives a command from the next downstream device.
+ */
+void cmdReceiveDownstream (void)
+{
+
+}
+
+/**
+ * Parses a command to determine if it should execute on this device.
+ */
+void cmdParse (command_t* command)
+{
+
+}
+
--- src/defs.h
+++ src/defs.h
@@ -1,3 +1,21 @@
 // defs.h
 // Contains type definitions and function stubs.
 
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdint.h>
+
+/* COMMAND (CMD) */
+
+typedef struct _command_t {
+    uint32_t id;  // TODO: What is the actual size required for this?
+
+    // TODO: What else goes in this struct?
+} command_t;
+
+void cmdSendUpstream (void);
+void cmdReceiveDownstream (void);
+void cmdParse (command_t* command);
+
+#endif  // DEFS_H
