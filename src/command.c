2052b3ed7147e43c27dc26bbeff80c844fc7f5f5
Jeremy Rempel <jeremy.rempel@usask.ca>
2021-12-24 14:27:29 -0600

    update docstrings for command module

--- src/command.c
+++ src/command.c
@@ -5,24 +5,28 @@
 
 /**
  * Sends a command to the next upstream device.
+ * @return void
  */
-void cmdSendUpstream (void)
+void cmdSendUpstream(void)
 {
 
 }
 
 /**
  * Receives a command from the next downstream device.
+ * @return void
  */
-void cmdReceiveDownstream (void)
+void cmdReceiveDownstream(void)
 {
 
 }
 
 /**
  * Parses a command to determine if it should execute on this device.
+ * @param command_t *command The command to be parsed
+ * @return void
  */
-void cmdParse (command_t* command)
+void cmdParse(command_t *command)
 {
 
 }
